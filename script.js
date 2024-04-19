7fe981a586efec1d5237f5a3a28852ee7a56ca2b
UniBreakfast <unibreakfast1@gmail.com>
2024-04-19 20:36:51 +0300

    implement third rule

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ setInterval(moveToNextGeneration, 500);
 /* 
 cell will be "alive" if cell on the left was "alive"
 cell will be "alive" if cell above was "alive" and there's no cell on the right,
+cell will be "alive" if cell on the right was "alive" and there's no cell below,
 */
 function moveToNextGeneration() {
   const currentGrid = board.tBodies[0];
@@ -14,10 +15,12 @@ function moveToNextGeneration() {
     const prevRow = currentGrid.rows[i - 1];
     const row = currentGrid.rows[i];
     const newRow = nextGrid.rows[i];
+    const nextRow = currentGrid.rows[i + 1];
 
     for (let j = 0; j < row.cells.length; j++) {
       const alive = row.cells[j - 1]?.classList.contains('alive') ||
-        prevRow?.cells[j]?.classList.contains('alive') && !row.cells[j + 1];
+      prevRow?.cells[j]?.classList.contains('alive') && !row.cells[j + 1] ||
+      row.cells[j + 1]?.classList.contains('alive') && !nextRow;
       
       newRow.cells[j].classList.toggle('alive', !!alive);
     }
