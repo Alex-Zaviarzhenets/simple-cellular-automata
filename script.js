052b47105012ecf1ed04ff69599ef18d31aaaafa
UniBreakfast <unibreakfast1@gmail.com>
2024-04-19 20:32:05 +0300

    implement first rule

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function toggleCellState(cellId) {
-  const cell = document.getElementById(cellId);
-  
-  cell.classList.toggle('alive');
-  cell.classList.toggle('dead');
-}
+const board = document.querySelector('.board');
 
-// Функция для переключения состояния клеток каждые 500 миллисекунд
-setInterval(function() {
-  toggleCellState('cell1');
-  toggleCellState('cell2');
-}, 500);
+setInterval(moveToNextGeneration, 500);
+
+/* 
+cell will be "alive" if cell on the left was "alive"
+*/
+function moveToNextGeneration() {
+  const currentGrid = board.tBodies[0];
+  const nextGrid = currentGrid.cloneNode(true);
+
+  for (let i = 0; i < currentGrid.rows.length; i++) {
+    const row = currentGrid.rows[i];
+    const newRow = nextGrid.rows[i];
+
+    for (let j = 0; j < row.cells.length; j++) {
+      const alive = !!row.cells[j - 1]?.classList.contains('alive');
+      
+      newRow.cells[j].classList.toggle('alive', alive);
+    }
+  }
+
+  currentGrid.replaceWith(nextGrid);
+}
