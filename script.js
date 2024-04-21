9df1dbc7b693d5385b62b1326def526c48f48aa6
Aliaksei-Zaviarzhenets <alex.zaviarzhenets12@gmail.com>
2024-04-21 17:15:40 +0200

    first  condition made

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 function toggleCellState(cellId) {
   const cell = document.getElementById(cellId);
-  
+
   cell.classList.toggle('alive');
   cell.classList.toggle('dead');
 }
+const board = document.querySelector('.board');
 
-// Функция для переключения состояния клеток каждые 500 миллисекунд
-setInterval(function() {
-  toggleCellState('cell1');
-  toggleCellState('cell2');
-}, 500);
+setInterval(updateNextGeneration, 500);
+
+function updateNextGeneration(){
+  const tableBody = board.tBodies[0]
+  const firstRow = tableBody.rows[0]
+  const newTableBody = tableBody.cloneNode(true);
+  
+  
+  
+  for (let i = 0; i < firstRow.cells.length; i++) {
+    const cell = firstRow.cells[i - 1];
+    const newCell = newTableBody.rows[0].cells[i];
+    console.log(newCell)
+    if (cell?.classList.contains('alive')) {
+      
+      newCell.classList.add('alive');
+      
+    } else {
+      newCell.classList.remove('alive');
+    }
+  }
+
+ 
+  tableBody.replaceWith(newTableBody);
+}
+
