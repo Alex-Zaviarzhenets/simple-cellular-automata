cc76cefd5ab57c02fea587578a01f8d06ec9b34e
Aliaksei-Zaviarzhenets <alex.zaviarzhenets12@gmail.com>
2024-04-21 18:43:51 +0200

    second rulles, but it does not work

--- script.js
+++ script.js
@@ -8,27 +8,41 @@ const board = document.querySelector('.board');
 
 setInterval(updateNextGeneration, 500);
 
-function updateNextGeneration(){
-  const tableBody = board.tBodies[0]
-  const firstRow = tableBody.rows[0]
+function updateNextGeneration() {
+  const tableBody = board.tBodies[0];
+  const rowCount = tableBody.rows.length;
+  const colCount = tableBody.rows[0].cells.length;
   const newTableBody = tableBody.cloneNode(true);
-  
-  
-  
-  for (let i = 0; i < firstRow.cells.length; i++) {
-    const cell = firstRow.cells[i - 1];
-    const newCell = newTableBody.rows[0].cells[i];
-    console.log(newCell)
-    if (cell?.classList.contains('alive')) {
-      
-      newCell.classList.add('alive');
-      
-    } else {
-      newCell.classList.remove('alive');
+
+  for (let i = 0; i < rowCount; i++) {
+    const row = tableBody.rows[i];
+    for (let j = 0; j < colCount - 1; j++) {
+      const currentCell = row.cells[j];
+      const leftCell = row.cells[j - 1];
+      const newCell = newTableBody.rows[i].cells[j];
+      if (leftCell?.classList.contains('alive')) {
+        newCell.classList.add('alive');
+      } else {
+        newCell.classList.remove('alive');
+      }
     }
   }
 
- 
+  for (let i = 0; i < rowCount; i++) {
+    const row = tableBody.rows[i];
+    const aboveCell = i > 0 ? tableBody.rows[i - 1].cells[colCount - 1] : null;
+    const rightCell = row.cells[colCount];
+    const newCell = newTableBody.rows[i].cells[colCount - 1];
+    if (aboveCell && aboveCell.classList.contains('alive') && !rightCell) {
+      newCell.classList.add('alive');
+    }
+    newCell.classList.remove('alive');
+  }
+
   tableBody.replaceWith(newTableBody);
 }
+
+
+
+
 
